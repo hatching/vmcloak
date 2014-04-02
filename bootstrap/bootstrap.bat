7acf8c829ac9f154e0b96e5b2ce48f4b1aef259d
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-04-02 23:35:00 +0200

    script and tools to install python on a clean image

---
+++ bootstrap/bootstrap.bat
@@ -0,0 +1,10 @@
+echo Installing Python
+start C:\Python-2.7.6.msi
+C:\click.exe "Python 2.7.6 Setup" "Next >"
+C:\click.exe "Python 2.7.6 Setup" "Next >"
+C:\click.exe "Python 2.7.6 Setup" "Next >"
+C:\click.exe "Python 2.7.6 Setup" "Finish"
+
+echo Cleaning up
+del C:\click.exe
+del C:\Python-2.7.6.msi
