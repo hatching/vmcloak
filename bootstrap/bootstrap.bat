f0a5c1bee51768e32a695cd0927fbd37645c1fb5
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-06-05 11:38:22 +0200

    assign a static ip address to the vm

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -1,14 +1,19 @@
-echo Installing Python
+echo Installing Python.
 start C:\Python-2.7.6.msi
 C:\click.exe "Python 2.7.6 Setup" "Next >"
 C:\click.exe "Python 2.7.6 Setup" "Next >"
 C:\click.exe "Python 2.7.6 Setup" "Next >"
 C:\click.exe "Python 2.7.6 Setup" "Finish"
 
-echo Cleaning up
+echo Cleaning up.
 del C:\click.exe
 del C:\Python-2.7.6.msi
 del C:\Windows\System32\nLite.cmd
+
+echo Setting static IP address.
+rem TODO Allow the static IP address to be configurable.
+netsh interface ip set address name="Local Area Connection" ^
+  static 192.168.56.101 255.255.255.0 192.168.0.1 1
 
 C:\Python27\Pythonw.exe C:\bootstrap.py
 start C:\Python27\Pythonw.exe C:\agent.py
