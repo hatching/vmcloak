ddb50cad61da3481498dece4359258f818a7f6c9
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-07-01 02:10:05 +0200

    add a couple of cuckoo headers

--- lib/deps.py
+++ lib/deps.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# Copyright (C) 2010-2014 Cuckoo Sandbox Developers.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
 from ConfigParser import ConfigParser
 import os.path
 import logging
