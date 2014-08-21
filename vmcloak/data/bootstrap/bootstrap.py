a5410750bb86d9baa0165231da1d4d5042ee109d
Thorsten Sick <thorsten.sick@avira.com>
2014-08-21 16:28:54 +0200

    Changing Hardware descripton registry keys

--- vmcloak/data/bootstrap/bootstrap.py
+++ vmcloak/data/bootstrap/bootstrap.py
@@ -4,9 +4,10 @@ import shutil
 import socket
 import subprocess
 import tempfile
+import random
 from _winreg import CreateKeyEx, SetValueEx
 from _winreg import HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE
-from _winreg import KEY_SET_VALUE, REG_DWORD
+from _winreg import KEY_SET_VALUE, REG_DWORD, REG_SZ, REG_MULTI_SZ
 
 from settings import HOST_IP, HOST_PORT, RESOLUTION
 
@@ -38,6 +39,36 @@ ENUM_CURRENT_SETTINGS = -1
 CDS_UPDATEREGISTRY = 1
 DISP_CHANGE_SUCCESSFUL = 0
 
+
+def generate_hd():
+    """ Generates random HD names
+    :return: a string with a  registry hd description
+    """
+    return " ".join(random.sample(["HD", "HARDDISK", "IBM", "WD", "Western Digital", "Seagate", "HGST", "Samsung",
+                                   "Harddisk", "Drive", "TB", "GB", "Desk", "IDE", "SATA", "USB", "Desktop"], 3))
+
+def generate_cd():
+    """ Generates random CD names
+    :return: a string with a  registry hd description
+    """
+    return " ".join(random.sample(["CD", "CDROM", "IBM", "WD", "Western Digital", "Seagate", "HGST", "Samsung",
+                                   "CD-ROM", "Drive", "TB", "GB", "Desk", "IDE", "SATA", "USB",
+                                   "BlueRay", "Blue-Ray", "DVD", "DVD-ROM", "DVD ROM", "RW"], 3))
+
+def generate_bios():
+    """ Generates random BIOS names
+    :return: a string with a  registry hd description
+    """
+    return " ".join(random.sample(["BIOS", "3.6", "5.4", "System", "Version", "Award", "AMI", "EFI", "UEFI",
+                                   "Insyde", "SeaBIOS"], 3))
+
+def generate_vga_bios():
+    """ Generates random VGA BIOS names
+    :return: a string with a  registry hd description
+    """
+    return " ".join(random.sample(["BIOS", "3.6", "5.24", "VGA", "Version", "Gigabyte", "Dell", "Sapphire", "Alienware",
+                                   "Gainward", "Asus"], 3))
+
 REGISTRY = [
     # Disable "Windows XP Tour" prompt.
     # http://www.techrepublic.com/article/tech-tip-disable-the-windows-xp-tour-prompt/
@@ -46,7 +77,23 @@ REGISTRY = [
     # Disable the Windows Indexation service.
     # http://www.wikihow.com/Turn-Off-Windows-XP%E2%80%99s-Indexing-Service
     (HKEY_LOCAL_MACHINE, 'System\\CurrentControlSet\\Services\\CiSvc', 'Start', REG_DWORD, 4),
-]
+
+    # Hide HD device identifier (pafish 1)
+    (HKEY_LOCAL_MACHINE, "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", 'Identifier', REG_SZ, generate_hd()),
+
+    # Hide CDROM (pafish 1+)
+    (HKEY_LOCAL_MACHINE, "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 1\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", 'Identifier', REG_SZ, generate_cd()),
+
+    # Hide SystemBios Version (pafish 2)
+    (HKEY_LOCAL_MACHINE, "HARDWARE\\Description\\System", "SystemBiosVersion", REG_MULTI_SZ, [generate_bios()]),
+
+    # Hide SystemBios Version (pafish 4)
+    (HKEY_LOCAL_MACHINE, "HARDWARE\\Description\\System", "VideoBiosVersion", REG_MULTI_SZ,
+     [generate_vga_bios(), generate_vga_bios()])
+    ]
+
+
+
 
 
 def set_resolution(width, height):
@@ -75,14 +122,13 @@ if __name__ == '__main__':
     # Read the agent.py file so we can drop it again later on.
     agent = open('C:\\vmcloak\\agent.py', 'rb').read()
 
-    # Remove all vmcloak files that are directly related. This does not
-    # include the auxiliary directory or any of its contents.
-    shutil.rmtree('C:\\vmcloak')
+    try:
+        s = socket.create_connection((HOST_IP, HOST_PORT))
 
-    s = socket.create_connection((HOST_IP, HOST_PORT))
-
-    width, height = [int(x) for x in RESOLUTION.split('x')]
-    s.send('\x01' if set_resolution(width, height) else '\x00')
+        width, height = [int(x) for x in RESOLUTION.split('x')]
+        s.send('\x01' if set_resolution(width, height) else '\x00')
+    except socket.error:
+        print("Socket error")
 
     for key, subkey, name, typ, value in REGISTRY:
         set_regkey(key, subkey, name, typ, value)
@@ -94,3 +140,7 @@ if __name__ == '__main__':
     # Don't wait for this process to end. Also, the agent will remove the
     # temporary agent file itself.
     subprocess.Popen(['C:\\Python27\\pythonw.exe', path])
+
+    # Remove all vmcloak files that are directly related. This does not
+    # include the auxiliary directory or any of its contents.
+    shutil.rmtree('C:\\vmcloak')
