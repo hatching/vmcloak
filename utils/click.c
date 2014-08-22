df54a6429a59dc56eb7d1f81e35bbfdeaca6c388
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-08-22 15:30:43 +0200

    click.c code cleanup

--- utils/click.c
+++ utils/click.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <windows.h>
 
-/** click a specific button in a specific window
-*
-**/
-int clickit(char * window, char * button) {
+int click_button(const char *window, const char *button)
+{
     int message = 0;
     while (1) {
         HWND window_handle = FindWindow(NULL, window);
         if(window_handle == NULL) {
-            if (message == 0) {
-                printf("Error: Failed to find window %s\n", window);
+            if(message == 0) {
+                printf("Error: Failed to find window '%s'.\n", window);
                 message = 1;
             }
             Sleep(50);
@@ -19,8 +17,9 @@ int clickit(char * window, char * button) {
 
         HWND button_handle = FindWindowEx(window_handle, NULL, NULL, button);
         if(button_handle == NULL) {
-            if (message == 0) {
-                printf("Error: failed to find window/button |%s| |%s|\n", window, button);
+            if(message == 0) {
+                printf("Error: failed to find window/button '%s' '%s'.\n",
+                    window, button);
                 message = 1;
             }
             Sleep(50);
@@ -28,12 +27,12 @@ int clickit(char * window, char * button) {
         }
 
         SetActiveWindow(window_handle);
-        if(IsWindowEnabled(button_handle)) {
+        if(IsWindowEnabled(button_handle) != FALSE) {
             SendMessage(button_handle, BM_CLICK, 0, 0);
             break;
         }
         else {
-            printf("Button not enabled\n");
+            printf("Waiting: Button not enabled yet.\n");
             Sleep(50);
             continue;
         }
@@ -42,90 +41,59 @@ int clickit(char * window, char * button) {
     return 0;
 }
 
-/* Print a button name
-*
-*/
-BOOL CALLBACK PrintButton(HWND hwnd, LPARAM lParam) {
-    LPTSTR name;
+BOOL CALLBACK print_title(HWND hwnd, LPARAM lParam)
+{
+    (void) lParam;
 
-    name = malloc(500*sizeof(TCHAR));
-    if (name != NULL){
-        GetWindowText(hwnd, name, 500);
-        if (strlen(name) > 0) {
-            printf("|%s|\n", name);
-        }
-        free(name);
+    char title[512];
+
+    GetWindowText(hwnd, title, sizeof(title));
+    if(strlen(title) != 0) {
+        printf("'%s'\n", title);
     }
 
     return TRUE;
 }
 
-/** Print a window name
-*
-**/
-BOOL CALLBACK PrintWindowName(HWND hwnd, LPARAM lParam) {
-    LPTSTR name;
-
-    name = malloc(500*sizeof(TCHAR));
-    if (name != NULL){
-        GetWindowText(hwnd, name, 500);
-        if (strlen(name)>0) {
-            printf("|%s|\n", name);
-        }
-        free(name);
-    }
-
-    return TRUE;
-}
-
-
-/** List all named buttons in a specific window
-*
-**/
-int find_buttons(char * window) {
+int enum_buttons(const char *window)
+{
     HWND window_handle = FindWindow(NULL, window);
     if(window_handle == NULL) {
-        printf("Error: failed to find window |%s|\n", window);
+        printf("Error: failed to find window '%s'\n", window);
         return 1;
     }
 
-    EnumChildWindows(window_handle, &PrintButton, 0);
+    EnumChildWindows(window_handle, &print_title, 0);
     return 0;
 }
 
-/** List all named windows
-*
-**/
-int find_windows() {
-
-    EnumChildWindows(NULL, &PrintWindowName, 0);
+int enum_windows()
+{
+    EnumChildWindows(NULL, &print_title, 0);
     return 0;
 }
 
-/** Main dispatcher
-*
-**/
 int main(int argc, char *argv[])
 {
-    if(argc == 3) {
-        clickit(argv[1], argv[2]);
-        return 0;
-    }
-    
-    else if (argc == 2) {
-        find_buttons(argv[1]);
-        return 0;
-    }
-
-    else if (argc == 1) {
-        find_windows();
-        return 0;
-    }
-
-    printf("Usage to click buttons: %s <window-title> <button-name>\n", argv[0]);
+    printf("Usage to click buttons: %s <window-title> <button-name>\n",
+        argv[0]);
     printf("Usage to identify buttons: %s <window-title>\n", argv[0]);
-    printf("Usage to identify windows: %s \n", argv[0]);
+    printf("Usage to identify windows: %s\n", argv[0]);
+
+    switch (argc) {
+    case 1:
+        enum_windows();
+        break;
+
+    case 2:
+        enum_buttons(argv[1]);
+        break;
+
+    case 3:
+        click_button(argv[1], argv[2]);
+        break;
+    }
+
     return 1;
 
 }
-
