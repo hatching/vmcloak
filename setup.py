2d9280a55f733c638a37e6577e7f659d4fda30e1
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-09-02 17:10:08 +0200

    version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='VMCloak',
-    version='0.1.0',
+    version='0.1.1',
     author='Jurriaan Bremer',
     author_email='jurriaanbremer@gmail.com',
     packages=[
