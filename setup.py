d835eacd154346d27879a67a666c321868f72257
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-10-21 09:45:03 +0200

    add --cuckoo ~/cuckoo to -r

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='VMCloak',
-    version='0.1.12',
+    version='0.1.13',
     author='Jurriaan Bremer',
     author_email='jurriaanbremer@gmail.com',
     packages=[
