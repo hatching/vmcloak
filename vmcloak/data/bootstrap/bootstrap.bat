b134afe186a9bad0d4bb886a6a61a8e13525a843
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-11-28 22:40:41 +0100

    hopefully re-introduce backwards compatibility

--- vmcloak/data/bootstrap/bootstrap.bat
+++ vmcloak/data/bootstrap/bootstrap.bat
@@ -3,6 +3,12 @@ call C:\vmcloak\settings.bat
 echo Setting the DNS Server IP address.
 netsh interface ip set dns name="Local Area Connection" static %DNSSERVER%
 
+if "%VMMODE" == "normal" {
+    echo Setting static IP address.
+    netsh interface ip set address name="Local Area Connection" ^
+        static %HOSTONLYIP% %HOSTONLYMASK% %HOSTONLYGATEWAY% 1
+)
+
 if "%BRIDGED%" == "yes" (
     netsh interface ip set address name="Local Area Connection 2" ^
         static %BRIDGEDIP% %BRIDGEDMASK% %BRIDGEDGATEWAY% 1
