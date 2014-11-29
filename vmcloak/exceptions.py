3f56435e467145f35908211d022019b784efee41
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-11-29 02:55:14 +0100

    forgot imported exceptions file

---
+++ vmcloak/exceptions.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2014 Jurriaan Bremer.
+# This file is part of VMCloak - http://www.vmcloak.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+class CommandError(Exception):
+    pass
