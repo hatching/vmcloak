7916186b18a4125f0b599febe37b7c719082d567
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-03-03 21:18:05 +0100

    updated copyright licenses

--- bin/vmcloak-gethwconf
+++ bin/vmcloak-gethwconf
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2014 Jurriaan Bremer.
+# Copyright (C) 2014-2015 Jurriaan Bremer.
 # This file is part of VMCloak - http://www.vmcloak.org/.
 # See the file 'docs/LICENSE.txt' for copying permission.
 
--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'VMCloak'
-copyright = u'2014, Jurriaan Bremer'
+copyright = u'2014-2015, Jurriaan Bremer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
