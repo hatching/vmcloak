83411201d1ce390866e8c05bf2949cf7a22bc87b
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-05-31 09:29:34 +0200

    remove unused files from vmcloak/utils/

--- vmcloak/utils/machete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Copyright (C) 2014-2015 Jurriaan Bremer.
-# This file is part of VMCloak - http://www.vmcloak.org/.
-# See the file 'docs/LICENSE.txt' for copying permission.
-
-# If everything else fails.. ;)
-killall -q -9 VirtualBox VBoxSVC VBoxXPCOMIPCD VBoxHeadless VBoxNetDHCP
