a5a096ceb38b11a1ba156781a9dd797d725ec36c
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-09-21 02:25:49 +0200

    some dependencies

---
+++ vmcloak/dependencies/pillow.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2014-2015 Jurriaan Bremer.
+# This file is part of VMCloak - http://www.vmcloak.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+from vmcloak.abstract import Dependency
+
+class Pillow(Dependency):
+    name = "pillow"
+    url = "http://cuckoo.sh/vmcloak/Pillow-2.9.0.win32-py2.7.exe"
+    sha1 = "1138f6db53b54943cbe7cf237c4df7e9255ca034"
+
+    def run(self):
+        self.upload_dependency("C:\\pillow.exe")
+        self.a.execute("C:\\pillow.exe", async=True)
+        self.a.click("Setup", "&Next >")
+        self.a.click("Setup", "&Next >")
+        self.a.click("Setup", "&Next >")
+        self.a.click("Setup", "Finish")
+        self.a.remove("C:\\pillow.exe")
---
+++ vmcloak/dependencies/wic.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2014-2015 Jurriaan Bremer.
+# This file is part of VMCloak - http://www.vmcloak.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+from vmcloak.abstract import Dependency
+
+class WIC(Dependency):
+    name = "wic"
+    url = "http://cuckoo.sh/vmcloak/wic_x86_enu.exe"
+    sha1 = "53c18652ac2f8a51303deb48a1b7abbdb1db427f"
+
+    def run(self):
+        self.upload_dependency("C:\\wic.exe")
+        self.a.execute("C:\\wic.exe /passive /norestart")
+        self.a.remove("C:\\wic.exe")
