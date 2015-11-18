fea79daf523db99f76052fb4f6ba0a9dcc3e0f67
Svieg <svieg.ashcasce@gmail.com>
2015-11-18 16:53:36 -0500

    Fixed hide.py PEP8

--- vmcloak/hide.py
+++ vmcloak/hide.py
@@ -13,33 +13,40 @@ class Hide(Object):
         self.a = a
 
     def load_json(file_name):
-    """ Takes a JSON file and returns the content in a dictionnary."""
+        """ Takes a JSON file and returns the content in a dictionnary."""
         file_object = open(file_name, "r")
         file_content = file_object.read()
         return loads(file_content)
 
     def modify_registry(key):
-    """ Takes a key dictionnary and applies the required reg command to modify the Windows Registry."""
+        """ Takes a key dictionnary and
+        applies the required reg command to modify the Windows Registry."""
         command = "reg {0} {1} /v {2} /t {3} /d {4}".format(
-            
                 key["modification_type"],
                 key["location"],
                 key["value"],
                 key["type"],
                 key["data"])
 
-        if !self.a.execute(command)
+        self.a.execute(command)
 
-            print "Invalid operation for the following key : {}".format(key["value"])
+        #    print "Invalid operation for the following key : {}".format(
+        # key["value"])
 
     def modify_directory(directory):
-    """ Takes a directory dictionnary and applies the required command to create or remove a directory. Uses the Agent directory commands."""
+        """ Takes a directory dictionnary,
+        Applies the required command to create or remove a directory.
+        Uses the Agent directory commands."""
         mod_type = directory["modifcation_type"]
 
-        if mod_type == "remove"
+        if mod_type == "remove":
 
             self.a.remove(directory["path"])
 
-        elif mod_type == "create"
+        elif mod_type == "rename":
+
+            self.a.rename(directory["path"])
+
+        elif mod_type == "create":
 
             raise
