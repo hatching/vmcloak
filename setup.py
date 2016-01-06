46eb75e2fb361a021016237064cc94f7785d73c5
Jurriaan Bremer <jbr@cuckoo.sh>
2016-01-06 01:37:32 +0100

    version 0.3.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='VMCloak',
-    version='0.3.6',
+    version='0.3.7',
     author='Jurriaan Bremer',
     author_email='jurriaanbremer@gmail.com',
     packages=[
