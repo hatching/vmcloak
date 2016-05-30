b2643075f2e1959ab4373a0d74b3969957a47b60
Jurriaan Bremer <jbr@cuckoo.sh>
2016-05-30 16:52:47 +0200

    abstract away differences between win7 x86/x64

--- vmcloak/win7.py
+++ vmcloak/win7.py
@@ -26,6 +26,8 @@ class Windows7(OperatingSystem):
     # List of preferences when multiple Windows 7 types are available.
     preference = "professional", "homepremium", "ultimate", "homebasic"
 
+    ARCH = None
+
     def _autounattend_xml(self, product):
         values = {
             'PRODUCTKEY': self.serial_key,
@@ -33,7 +35,7 @@ class Windows7(OperatingSystem):
             'USERNAME': random_string(8, 12),
             'PASSWORD': random_string(8, 16),
             "PRODUCT": product.upper(),
-            "ARCH": 'x86',
+            "ARCH": self.ARCH,
         }
 
         buf = open(os.path.join(self.path, 'autounattend.xml'), 'rb').read()
@@ -90,78 +92,7 @@ class Windows7(OperatingSystem):
         return True
 
 class Windows7x64(OperatingSystem):
-    name = 'win7'
-    service_pack = 2
-    mount = '/mnt/win7'
-    nictype = '82540EM'
-    osdir = os.path.join('sources', '$oem$', '$1')
-    genisoargs = [
-        '-no-emul-boot', '-iso-level', '2', '-udf', '-J', '-l', '-D', '-N',
-        '-joliet-long', '-relaxed-filenames',
-    ]
+    ARCH = "amd64"
 
-    # List of preferences when multiple Windows 7 types are available.
-    preference = "professional", "homepremium", "ultimate", "homebasic"
-
-    def _autounattend_xml(self, product):
-        values = {
-            'PRODUCTKEY': self.serial_key,
-            'COMPUTERNAME': random_string(8, 14),
-            'USERNAME': random_string(8, 12),
-            'PASSWORD': random_string(8, 16),
-            "PRODUCT": product.upper(),
-            "ARCH": 'amd64',
-        }
-
-        buf = open(os.path.join(self.path, 'autounattend.xml'), 'rb').read()
-        for key, value in values.items():
-            buf = buf.replace('@%s@' % key, value)
-
-        return buf
-
-    def isofiles(self, outdir, tmp_dir=None):
-        products = []
-
-        product_ini = os.path.join(outdir, "sources", "product.ini")
-        mode, conf = ini_read(product_ini)
-
-        for line in conf.get("BuildInfo", []):
-            if "=" not in line:
-                continue
-
-            key, value = line.split("=", 1)
-            if key != "staged":
-                continue
-
-            for product in value.split(","):
-                products.append(product.lower())
-
-            break
-
-        for preference in self.preference:
-            if preference in products:
-                product = preference
-                break
-        else:
-            if products:
-                product = products[0]
-            else:
-                product = self.preference[0]
-
-        if self.s.product and self.s.product.lower() not in self.preference:
-            log.error("The product version of Windows 7 that was specified "
-                      "on the command-line is not known by us, ignoring it.")
-            self.s.product = None
-
-        with open(os.path.join(outdir, 'autounattend.xml'), 'wb') as f:
-            f.write(self._autounattend_xml(self.s.product or product))
-
-    def set_serial_key(self, serial_key):
-        if serial_key and not valid_serial_key(serial_key):
-            log.error('The provided serial key has an incorrect encoding.')
-            log.info('Example encoding, AAAAA-BBBBB-CCCCC-DDDDD-EEEEE.')
-            return False
-
-        # https://technet.microsoft.com/en-us/library/jj612867.aspx
-        self.serial_key = serial_key or '33PXH-7Y6KF-2VJC9-XBBR8-HVTHH'
-        return True
+class Windows7x86(OperatingSystem):
+    ARCH = "x86"
