33f1ec74ab0bf59ef21fbfbc200c215dd337aece
razu <rasmus@cert.ee>
2016-06-15 18:23:39 +0300

    IE10 install almost works

--- vmcloak/dependencies/ie.py
+++ vmcloak/dependencies/ie.py
@@ -9,21 +9,18 @@ from vmcloak.abstract import Dependency
 
 class IE(Dependency):
     name = "ie"
-    default = "11_x64"
+    default = "10_x64"
+    depends = ["KB:2729094", "KB:2731771", "KB:2533623", "KB:2670838", "KB:2786081", "KB:2639308", "KB:2834140"]
     exes = [
         {
             "version": "11_x64",
             "url": "http://download.microsoft.com/download/7/1/7/7179A150-F2D2-4502-9D70-4B59EA148EAA/IE11-Windows6.1-x64-en-us.exe",
             "sha1": "ddec9ddc256ffa7d97831af148f6cc45130c6857",
-            "depends": ["KB2729094", "KB2731771", "KB2533623", "KB2670838", "KB2786081"], 
         },
         {
             "version": "10_x64",
             "url": "http://download.microsoft.com/download/C/E/0/CE0AB8AE-E6B7-43F7-9290-F8EB0EA54FB5/IE10-Windows6.1-x64-en-us.exe",
             "sha1": "17d1eaca123e453269b12b20863fd5ce96727888",
-            "requires": [
-
-            ]
         },
     ]
 
@@ -31,6 +28,5 @@ class IE(Dependency):
 
         self.upload_dependency("C:\\setup.exe")
         self.a.execute("C:\\setup.exe /quiet /norestart /update-no")
-        
-        self.wait_process_exit("IE10-Windows6.1-x64-en-us.exe")
-        
+
+        self.a.remove("C:\\setup.exe")
