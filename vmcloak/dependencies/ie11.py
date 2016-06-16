77447fbc6c84f8a4289757070bd4cc1e1c924dd0
razu <rasmus@cert.ee>
2016-06-16 16:28:22 +0300

    split IE until we find a better way of handling depends

--- vmcloak/dependencies/ie11.py
+++ vmcloak/dependencies/ie11.py
@@ -7,22 +7,16 @@ import time
 
 from vmcloak.abstract import Dependency
 
-class IE(Dependency):
-    name = "ie"
-    default = "11_x64"
+class IE11(Dependency):
+    name = "ie11"
+    default = "11"
     depends = ["KB:2670838", "KB:2639308", "KB:2533623", "KB:2731771", "KB:2729094", "KB:2786081", "KB:2882822", "KB:2888049", "KB:2834140"]
     exes = [
         {
-            "version": "11_x64",
+            "version": "11",
+            "target": "win7x64",
             "url": "http://download.microsoft.com/download/7/1/7/7179A150-F2D2-4502-9D70-4B59EA148EAA/IE11-Windows6.1-x64-en-us.exe",
             "sha1": "ddec9ddc256ffa7d97831af148f6cc45130c6857",
-            "depends": ["KB:2670838", "KB:2639308", "KB:2533623", "KB:2731771", "KB:2729094", "KB:2786081", "KB:2882822", "KB:2888049", "KB:2834140"],
-        },
-        {
-            "version": "10_x64",
-            "url": "http://download.microsoft.com/download/C/E/0/CE0AB8AE-E6B7-43F7-9290-F8EB0EA54FB5/IE10-Windows6.1-x64-en-us.exe",
-            "sha1": "17d1eaca123e453269b12b20863fd5ce96727888",
-            "depends": ["KB:2729094", "KB:2731771", "KB:2533623", "KB:2670838", "KB:2786081", "KB:2639308", "KB:2834140"],
         },
     ]
 
