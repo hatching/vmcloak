0465429f8fa778b9f5ad009bc126a809d117345b
razu <rasmus@cert.ee>
2016-07-07 22:46:14 +0300

    merge from master

--- vmcloak/repository.py
+++ vmcloak/repository.py
@@ -13,6 +13,7 @@ conf_path = os.path.join(os.getenv("HOME"), ".vmcloak")
 image_path = os.path.join(conf_path, "image")
 vms_path = os.path.join(conf_path, "vms")
 deps_path = os.path.join(conf_path, "deps")
+iso_dst_path = os.path.join(conf_path, "iso")
 
 repository = os.path.join(conf_path, "repository.db")
 engine = create_engine("sqlite:///%s" % repository)
@@ -25,6 +26,7 @@ class Image(Base):
 
     id = Column(Integer, primary_key=True)
     mode = Column(String(16))
+    vm = Column(String(16))
     name = Column(String(64))
     path = Column(Text)
     osversion = Column(String(32))
@@ -58,3 +60,6 @@ if not os.path.isdir(image_path):
 
 if not os.path.isdir(deps_path):
     os.mkdir(deps_path)
+
+if not os.path.isdir(iso_dst_path):
+    os.mkdir(iso_dst_path)
