fc9fbd06565f701d37de8e7f0c486b36ca0eed16
RicoVZ <Ricardo.van.Zutphen@hva.nl>
2016-07-22 03:53:57 +0200

    Add visual studio redistributable dependency

---
+++ vmcloak/dependencies/vcredist.py
@@ -0,0 +1,86 @@
+# Copyright (C) 2014-2016 Jurriaan Bremer.
+# This file is part of VMCloak - http://www.vmcloak.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+from vmcloak.abstract import Dependency
+
+
+class VcRedist(Dependency):
+    name = "vcredist"
+    description = "Visual studio redistributable packages"
+    default = "2005_x86"
+
+    install_params = {
+        "2005_x86": "/q:a",
+        "2005_x64": "/q:a",
+        "2008_x86": "/qb",
+        "2008_x64": "/qb",
+        "2010_x86": "/passive /norestart",
+        "2010_x64": "/passive /norestart",
+        "2012_x86": "/passive /norestart",
+        "2012_x64": "/passive /norestart",
+        "2013_x86": "/passive /norestart",
+        "2013_x64": "/passive /norestart"
+    }
+
+    exes = [
+        {
+            "version": "2005_x86",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2005_x86.exe",
+            "sha1": "7dfa98be78249921dd0eedb9a3dd809e7d215c8d"
+        },
+        {
+            "version": "2005_x64",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2005_x64.exe",
+            "sha1": "756f2c773d4733e3955bf7d8f1e959a7f5634b1a"
+        },
+        {
+            "version": "2008_x86",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2008_x86.exe",
+            "sha1": "6939100e397cef26ec22e95e53fcd9fc979b7bc9"
+        },
+        {
+            "version": "2008_x64",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2008_x64.exe",
+            "sha1": "13674c43652b941dafd2049989afce63cb7c517b"
+        },
+        {
+            "version": "2010_x86",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2010_x86.exe",
+            "sha1": "b84b83a8a6741a17bfb5f3578b983c1de512589d"
+        },
+        {
+            "version": "2010_x64",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2010_x64.exe",
+            "sha1": "027d0c2749ec5eb21b031f46aee14c905206f482"
+        },
+        {
+            "version": "2012_x86",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2012_x86.exe",
+            "sha1": "7d6f654c16f9ce534bb2c4b1669d7dc039c433c9"
+        },
+        {
+            "version": "2012_x64",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2012_x64.exe",
+            "sha1": "c4ac45564e801e1bfd87936cac8a76c5754cdbd4"
+        },
+        {
+            "version": "2013_x86",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2013_x86.exe",
+            "sha1": "df7f0a73bfa077e483e51bfb97f5e2eceedfb6a3"
+        },
+        {
+            "version": "2013_x64",
+            "url": "http://cuckoo.sh/vmcloak/vcredist_2013_x64.exe",
+            "sha1": "8bf41ba9eef02d30635a10433817dbb6886da5a2"
+        }
+    ]
+
+    def run(self):
+        # Get the installation parameters required to start
+        # an unattended install
+        param = self.install_params[self.version]
+
+        self.upload_dependency("C:\\vcredist.exe")
+        self.a.execute("C:\\vcredist.exe %s" % param)
+        self.a.remove("C:\\vcredist.exe")
