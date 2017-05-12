7aa10a5ebf4ba209473fa24b8fd18452892e11a7
Jop van der Lelie <jop@honeyned.nl>
2017-05-12 16:09:02 +0200

    weaken IE11 security

--- vmcloak/dependencies/ie11.py
+++ vmcloak/dependencies/ie11.py
@@ -36,3 +36,99 @@ class IE11(Dependency):
         self.upload_dependency("C:\\setup.exe")
         self.a.execute("C:\\setup.exe /quiet /norestart /update-no")
         self.a.remove("C:\\setup.exe")
+
+        # disable first use popup:
+        # http://www.geoffchappell.com/notes/windows/ie/firstrun.htm
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\" "
+            "/v DisableFirstRunCustomize /t REG_DWORD /d 1 /f"
+        )
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\"
+            "Microsoft\\Internet Explorer\\Main\" "
+            "/v RunOnceComplete /t REG_DWORD /d 1 /f"
+        )
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\"
+            "Microsoft\\Internet Explorer\\Main\" "
+            "/v RunOnceHasShown /t REG_DWORD /d 1 /f"
+        )
+
+        # disable auto update:
+        # https://social.technet.microsoft.com/Forums/en-US/c49c96f7-247e-4404-b80f-3df5253dc13f/how-to-uncheck-install-new-versions-automatically-from-internet-explorer-11-from-sccm-2012?forum=ieitprocurrentver
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\"
+            "Microsoft\\Internet Explorer\\Main\" "
+            "/v EnableAutoUpgrade /t REG_DWORD /d 0 /f"
+        )
+
+        # disable protected mode for all zones for all users
+        # https://superuser.com/questions/1031225/what-is-the-registry-setting-to-enable-protected-mode-in-a-specific-zone
+        for zone in range(0, 5):
+            self.a.execute(
+                "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+                "Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Zones\\%s\" "
+                "/v 2500 /t REG_DWORD /d 3 /f" % zone
+            )
+
+        # Disable protected mode
+        # https://www.eightforums.com/tutorials/31977-internet-explorer-enhanced-protected-mode-turn-off.html
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\" "
+            "/v Isolation /t REG_SZ /d \"PMIL\" /f"
+        )
+
+        # weaken IE11 security
+        # http://www.geoffchappell.com/notes/windows/ie/featurecontrol.htm
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\\"
+            "FeatureControl\\FEATURE_LOCALMACHINE_LOCKDOWN\" "
+            "/v \"iexplore.exe\" /t REG_DWORD /d 0 /f"
+        )
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\\"
+            "FeatureControl\\FEATURE_RESTRICT_FILEDOWNLOAD\" "
+            "/v \"iexplore.exe\" /t REG_DWORD /d 0 /f"
+        )
+
+        # Disable "fix security settings" warning
+        # https://answers.microsoft.com/en-us/ie/forum/ie8-windows_other/when-opening-internet-explorer-constantly-getting/e591c609-1e50-4210-a770-2474beb1430f?auth=1
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\\Security\" "
+            "/v DisableFixSecuritySettings /t REG_DWORD /d 1 /f"
+        )
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\\Security\" "
+            "/v DisableSecuritySettingsCheck /t REG_DWORD /d 1 /f"
+        )
+
+        # Disable IE8 IE9+ smartscreen filter:
+        # https://www.sevenforums.com/tutorials/1406-internet-explorer-smartscreen-filter-turn-off.html
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\PhishingFilter\" "
+            "/v EnabledV8 /t REG_DWORD /d 0 /f"
+        )
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\PhishingFilter\" "
+            "/v EnabledV9 /t REG_DWORD /d 0 /f"
+        )
+
+        # set default webpage and disable IE default browser check:
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\" "
+            "/v \"Check_Associations\" /t REG_SZ /d \"no\" /f"
+        )
+        self.a.execute(
+            "reg add \"HKEY_LOCAL_MACHINE\\Software\\Policies\\"
+            "Microsoft\\Internet Explorer\\Main\" "
+            "/v \"Start Page\" /t REG_SZ /d \"about:blank\" /f"
+        )
