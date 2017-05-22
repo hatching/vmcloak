c9ad28ec863728cf85d50c87f382b099a1036acb
Jop van der Lelie <jop@honeyned.nl>
2017-05-22 16:17:48 +0200

    Add more versions of adobe flash player.

--- vmcloak/dependencies/flash.py
+++ vmcloak/dependencies/flash.py
@@ -20,6 +20,7 @@ class Flash(Dependency):
             "sha1": "906f920563403ace6dee9b0cff982ea02d4b5c06",
         },
         {
+            # Released 4/9/2013
             "version": "11.7.700.169",
             "url": "https://cuckoo.sh/vmcloak/flashplayer11_7r700_169_winax.msi",
             "sha1": "790b09c63c44f0eafd7151096bf58964857d3b17",
@@ -45,6 +46,7 @@ class Flash(Dependency):
             "sha1": "877c7e9a6de692bdef95a4f3cc22b9fb385db92e",
         },
         {
+            # Released 1/14/2014
             "version": "12.0.0.38",
             "url": "https://cuckoo.sh/vmcloak/flashplayer12_0r0_38_winax.msi",
             "sha1": "e44908c9a813876c1a332a174b2e3e9dff47a4ff",
@@ -105,9 +107,112 @@ class Flash(Dependency):
             "sha1": "1efef336190b021f6df3325f958f1698a91cce8c",
         },
         {
+            # Released 12/8/2015
             "version": "20.0.0.228",
             "url": "https://cuckoo.sh/vmcloak/flashplayer20_0d0_228_winax.msi",
             "sha1": "fa86f107111fc7def35742097bf0aa29c82d7638",
+        },
+        {
+            # Released 1/19/2016
+            "version": "20.0.0.286",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer20_0r0_286_winax.msi",
+            "sha1": "a1b73c662b7bce03ff2d8a729005e81036ea1a24",
+        },
+        {
+            # Released 2/9/2016
+            "version": "20.0.0.306",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer20_0r0_306_winax.msi",
+            "sha1": "68ae4277d68146749cb6cf597d3d6be07422c372",
+        },
+        {
+            # Released 3/10/2016
+            "version": "21.0.0.182",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer21_0r0_182_winax.msi",
+            "sha1": "717582fc7138c9cf4b3ec88413d38cddf8613d4c",
+        },
+        {
+            # Released 3/23/2016
+            "version": "21.0.0.197",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer21_0r0_197_winax.msi",
+            "sha1": "1e81a4617abcbb95ee6bf9250be5a205ce0d289b",
+        },
+        {
+            # Released 4/7/2016
+            "version": "21.0.0.213",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer21_0r0_213_winax.msi",
+            "sha1": "6d23ae1f12682b6baa1833c12955ca9adf7d13f2",
+        },
+        {
+            # Released 5/12/2016
+            "version": "21.0.0.242",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer21_0r0_242_winax.msi",
+            "sha1": "01261660476cdcacf1872a8a5142018bce6de1dc",
+        },
+        {
+            # Released 6/16/2016
+            "version": "22.0.0.192",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer22_0r0_192_winax.msi",
+            "sha1": "6aba77bca04f7170ac90c3852b837027f840b051",
+        },
+        {
+            # Released 7/12/2016
+            "version": "22.0.0.209",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer22_0r0_209_winax.msi",
+            "sha1": "7db31a028cf14bdcdb4374fd941c08412130e384",
+        },
+        {
+            # Released 9/13/2016
+            "version": "23.0.0.162",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer23_0r0_162_winax.msi",
+            "sha1": "4408e4b6c9ced6490d2b0b81d6a62d00776c8042",
+        },
+        {
+            # Released 10/11/2016
+            "version": "23.0.0.185",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer23_0r0_185_winax.msi",
+            "sha1": "b86c4a79c445481992ac41c47a22323068ca40b1",
+        },
+        {
+            # Released 10/26/2016
+            "version": "23.0.0.205",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer23_0r0_205_winax.msi",
+            "sha1": "c329a24860028263631411bd1c37369022473e9a",
+        },
+        {
+            # Released 11/8/2016
+            "version": "23.0.0.207",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer23_0r0_207_winax.msi",
+            "sha1": "699097fa5bbc4d3a0a9733108678ee642d1cc667",
+        },
+        {
+            # Released 12/13/2016
+            "version": "24.0.0.186",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer24_0r0_186_winax.msi",
+            "sha1": "f9828b0e872f71879664078dbaa23b05fff47494",
+        },
+        {
+            # Released 1/10/2017
+            "version": "24.0.0.194",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer24_0r0_194_winax.msi",
+            "sha1": "2d9648adf6fdc05cbc61b037ec5e434c7a73dfe4",
+        },
+        {
+            # Released 2/14/2017
+            "version": "24.0.0.221",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer24_0r0_221_winax.msi",
+            "sha1": "e8ac1c9c4e12ce7164b1f7374e6ef2868ab51e7f",
+        },
+        {
+            # Released 03/14/2017
+            "version": "25.0.0.127",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer25_0r0_127_winax.msi",
+            "sha1": "78618c96a0e65cc1ace1aedb0ecac04972b3ac7f",
+        },
+        {
+            # Released 04/11/2017
+            "version": "25.0.0.148",
+            "url": "https://cuckoo.sh/vmcloak/flashplayer25_0r0_148_winax.msi",
+            "sha1": "e3907d0e0ac26a2db556ee6fa864243f3cb0a9f0",
         },
         {
             "version": "latest",
