07e8c0059397b1c95dbcc074ab9270d96adfb2f9
Jop van der Lelie <jop@honeyned.nl>
2017-06-06 12:51:01 +0200

    Added some references for IE11.

--- vmcloak/dependencies/ie11.py
+++ vmcloak/dependencies/ie11.py
@@ -63,6 +63,12 @@ class IE11(Dependency):
             "/v EnableAutoUpgrade /t REG_DWORD /d 0 /f"
         )
 
+        #   0        My Computer
+        #   1        Local Intranet Zone
+        #   2        Trusted sites Zone
+        #   3        Internet Zone
+        #   4        Restricted Sites Zone
+        # https://support.microsoft.com/en-us/help/182569/internet-explorer-security-zones-registry-entries-for-advanced-users
         # disable protected mode for all zones for all users
         # https://superuser.com/questions/1031225/what-is-the-registry-setting-to-enable-protected-mode-in-a-specific-zone
         for zone in range(0, 5):
