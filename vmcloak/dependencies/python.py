acec189daeceb1ba58d1bfb14885a28a2d7d79a6
Jop van der Lelie <jop@honeyned.nl>
2017-06-07 12:40:48 +0200

    Add more info for python installer.

--- vmcloak/dependencies/python.py
+++ vmcloak/dependencies/python.py
@@ -16,6 +16,9 @@ class Python(Dependency):
                 "https://www.python.org/ftp/python/2.7.6/python-2.7.6.amd64.msi",
             ],
             "sha1": "405290650b85042f389a8fbf06549c35458afd05",
+            "filename": "python-2.7.6.amd64.msi",
+            "window_name": "Python 2.7.6 (64-bit) Setup",
+            "install_path": "C:\\Python27",
         },
         {
             "arch": "x86",
@@ -25,6 +28,9 @@ class Python(Dependency):
                 "https://cuckoo.sh/vmcloak/python-2.7.6.msi",
             ],
             "sha1": "c5d71f339f7edd70ecd54b50e97356191347d355",
+            "filename": "python-2.7.6.msi",
+            "window_name": "Python 2.7.6 Setup",
+            "install_path": "C:\\Python27",
         },
         {
             "arch": "amd64",
@@ -33,6 +39,9 @@ class Python(Dependency):
                 "https://www.python.org/ftp/python/2.7.13/python-2.7.13.amd64.msi",
             ],
             "sha1": "d9113142bae8829365c595735e1ad1f9f5e2894c",
+            "filename": "python-2.7.13.amd64.msi",
+            "window_name": "Python 2.7.13 (64-bit) Setup",
+            "install_path": "C:\\Python27",
         },
         {
             "arch": "x86",
@@ -41,6 +50,9 @@ class Python(Dependency):
                 "https://www.python.org/ftp/python/2.7.13/python-2.7.13.msi",
             ],
             "sha1": "7e3b54236dbdbea8fe2458db501176578a4d59c0",
+            "filename": "python-2.7.13.msi",
+            "window_name": "Python 2.7.13 Setup",
+            "install_path": "C:\\Python27",
         },
     ]
 
