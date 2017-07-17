a8ff9a22e3b6cf002e6d23270676417dbe49bcdd
Marc Ohm <ohm@cs.uni-bonn.de>
2017-07-17 11:36:39 +0200

    add a way to install costum executables

---
+++ vmcloak/dependencies/costum.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2014-2016 Jurriaan Bremer.
+# This file is part of VMCloak - http://www.vmcloak.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+# Costum Dependency submitted by Marc Ohm.
+
+import os.path
+
+from vmcloak.abstract import Dependency
+
+class Costum(Dependency):
+    name = "costum"
+    
+    def init(self):
+        self.exepath = None
+        self.params = None
+
+    def check(self):    
+        if not self.exepath or not os.path.isfile(self.exepath):
+            log.error("Please provide the path to the exe file.")
+            return False
+
+    def run(self):
+        self.a.upload(
+            "C:\\setup.exe",
+            open(self.exepath, "rb").read()
+        )
+        self.a.execute("C:\\setup.exe " + self.params if self.params else "")
+        self.a.remove("C:\\setup.exe")
