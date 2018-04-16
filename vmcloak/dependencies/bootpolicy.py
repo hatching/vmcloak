350c7dac1319569f8097b097440165a7961e37a3
Ben de Graaff <ben@nx3d.org>
2018-04-16 13:57:07 +0200

    Disable automatic reboot on BSoD when debugging
    Also fixes execution of 64-bit bcdedit from 32-bit process

--- vmcloak/dependencies/bootpolicy.py
+++ vmcloak/dependencies/bootpolicy.py
@@ -1,9 +1,13 @@
 # Copyright (C) 2018 Jurriaan Bremer.
 # This file is part of VMCloak - http://www.vmcloak.org/.
 # See the file 'docs/LICENSE.txt' for copying permission.
-# Chrome Dependency submitted by Jason Lewis.
 
+from __future__ import print_function
 from vmcloak.abstract import Dependency
+
+WMIC = "C:\\Windows\\sysnative\\wbem\\wmic.exe"
+BCDEDIT = "C:\\Windows\\sysnative\\bcdedit.exe"
+
 
 class BootPolicy(Dependency):
     name = "bootpolicy"
@@ -14,6 +18,10 @@ class BootPolicy(Dependency):
 
     def run(self):
         if self.debug == "1":
-            self.a.execute("bcdedit /debug on")
+            self.a.execute("%s /debug on" % BCDEDIT)
+            self.a.execute("%s RecoverOS set AutoReboot = false" % WMIC)
+            # Disable memory dump
+            self.a.execute("%s RecoverOS set DebugInfoType = 0" % WMIC)
 
-        self.a.execute("bcdedit /set {current} bootstatuspolicy ignoreallfailures")
+        self.a.execute("%s /timeout 1" % BCDEDIT)
+        self.a.execute("%s /set {current} bootstatuspolicy ignoreallfailures" % BCDEDIT)
