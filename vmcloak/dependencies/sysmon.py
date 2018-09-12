69e7ea6757c725f0c2bd466d50df16f9beab8694
haam3r <andres@cert.ee>
2018-09-12 11:46:57 +0300

    Add 8.0.0 as new version instead of replacing 6.0.2
    @samwakel pointed out that old versions should also be kept, so thanks

--- vmcloak/dependencies/sysmon.py
+++ vmcloak/dependencies/sysmon.py
@@ -9,14 +9,24 @@ class Sysmon(Dependency):
     description = "Monitoring and logging system activity"
     default = "8.0.0"
     exes = [{
-        "version": "8.0.0",
+        "version": "6.0.2",
         "arch": "amd64",
         "url": "https://cuckoo.sh/vmcloak/Sysmon64.exe",
+        "sha1": "ce9edbea1937d593bf6cba4d9ca57d66f1680fdf",
+    }, {
+        "version": "6.0.2",
+        "arch": "x86",
+        "url": "https://cuckoo.sh/vmcloak/Sysmon.exe",
+        "sha1": "b97d720eca991bd96d6a8d60ea93ee163e09178d",
+    }, {
+        "version": "8.0.0",
+        "arch": "amd64",
+        "url": "https://cuckoo.sh/vmcloak/Sysmon64-8.0.0.exe",
         "sha1": "173014bedc7d852a4cee961440ffaf06fa716353",
     }, {
         "version": "8.0.0",
         "arch": "x86",
-        "url": "https://cuckoo.sh/vmcloak/Sysmon.exe",
+        "url": "https://cuckoo.sh/vmcloak/Sysmon-8.0.0.exe",
         "sha1": "8cc70223e7f667387d9792a5183129734d3b5501",
     }]
 
