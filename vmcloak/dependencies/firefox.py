b3b05ccaf573868870fc618dace53ba2c206a270
Jurriaan Bremer <jbr@hatching.io>
2018-12-10 11:46:56 +0100

    use filenames without whitespaces

--- vmcloak/dependencies/firefox.py
+++ vmcloak/dependencies/firefox.py
@@ -14,11 +14,11 @@ class Firefox(Dependency):
         "sha1": "c5118ca76f0cf6ecda5d2b9292bf191525c9627a",
     }, {
         "version": "60.0.2",
-        "url": "https://cuckoo.sh/vmcloak/Firefox Setup 60.0.2.exe",
+        "url": "https://cuckoo.sh/vmcloak/firefox_60_0_2.exe",
         "sha1": "565c5ddca3e4acbc30a550f312d3a1a7fd8d8bce",
     }, {
         "version": "63.0.3",
-        "url": "https://cuckoo.sh/vmcloak/Firefox Setup 63.0.3.exe",
+        "url": "https://cuckoo.sh/vmcloak/firefox_63_0_3.exe",
         "sha1": "c5f03fc93aebd2db9da14ba6eb1f01e98e18d95b",
     }]
 
